c6ce3e6abe78a5c589939b2409bf93746b82a621
Dawid Barsznica <dbarsznica@gmail.com>
2019-01-30 00:00:33 +0100

    Add personal-data page

--- js/script.js
+++ js/script.js
@@ -6,3 +6,14 @@ document.querySelector('.navbutton').addEventListener('click', function (e) {
 function toggleMenu(visible) {
   $('.sidebar').addClass('jsss').removeClass('sidebar');
 }
+
+var check = function() {
+  if (document.getElementById('password').value ==
+    document.getElementById('retype').value) {
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').innerHTML = 'matching';
+    } else {
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').innerHTML = 'not matching';
+    }
+  }
